dce39b8d83b68f88a4b3cae3593afaeb0a090711
NEXOVATE <nexovatecgec@gmail.com>
2025-08-18 14:07:41 +0530

    added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,7 @@
-
 import { Container } from "react-bootstrap";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -27,7 +25,7 @@ import gallery11 from "../assets/Events/eureka25/11.jpeg";
 import gallery12 from "../assets/Events/eureka25/12.jpeg";
 import gallery13 from "../assets/Events/eureka25/13.jpeg";
 import gallery14 from "../assets/Events/eureka25/14.jpeg";
-import gallery15 from "../assets/Events/eureka25/5.jpeg";
+// import gallery15 from "../assets/Events/eureka25/5.jpeg";
 import gallery16 from "../assets/Events/eureka25/16.jpeg";
 import gallery17 from "../assets/Events/eureka25/17.jpeg";
 import gallery18 from "../assets/Events/eureka25/18.jpeg";
@@ -40,32 +38,29 @@ const galleryImages = [
   { src: gallery5, caption: "Lecture" },
   { src: gallery6, caption: "Lecture" },
   { src: gallery7, caption: "Lecture" },
-   { src: gallery11, caption: "Judges" },
-   { src: gallery18, caption: "Team 1" },
-    { src: gallery17, caption: "Team 2" },
-     { src: gallery16, caption: "Team 3" },
-     { src: gallery15, caption: "Team 4" },
-     { src: gallery14, caption: "Team 5" },
-     { src: gallery13, caption: "Team 6 " },
-     { src: gallery12, caption: "Team 7" },
-     { src: gallery10, caption: "Team 8" },
-     { src: gallery9, caption: "Team 9" },
-  { src: gallery8, caption: "Team 10" },
- 
- 
- 
-  
+  { src: gallery11, caption: "Judges" },
+  { src: gallery18, caption: "Team 1" },
+  { src: gallery17, caption: "Team 2" },
+  { src: gallery16, caption: "Team 3" },
+  //  { src: gallery15, caption: "Team 4" },
+  { src: gallery14, caption: "Team 4" },
+  { src: gallery13, caption: "Team 5 " },
+  { src: gallery12, caption: "Team 6" },
+  { src: gallery10, caption: "Team 7" },
+  { src: gallery9, caption: "Team 8" },
+  { src: gallery8, caption: "Team 9" },
 ];
 
 const Gallery = () => (
   <section id="gallery" className="py-5">
     <Container>
-      <h2 className="text-center fw-bold mb-5" >
-        Our <span className="text-primary ">Events</span>
+      <h2 className="text-center fw-bold mb-5">
+        Our <span className="text-primary ">Event gallery</span>
       </h2>
       <h2 className="text-center fw-bold mb-5 gallery-heading">
-        <span className="text-warning"><u>EUREKA-2K25</u></span>
-     
+        <span className="text-warning">
+          <u>EUREKA-2K25</u>
+        </span>
       </h2>
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
@@ -74,9 +69,9 @@ const Gallery = () => (
         autoplay={{ delay: 2500, disableOnInteraction: false }}
         spaceBetween={20}
         breakpoints={{
-          320: { slidesPerView: 1 }, 
-          576: { slidesPerView: 2 }, 
-          768: { slidesPerView: 3 }, 
+          320: { slidesPerView: 1 },
+          576: { slidesPerView: 2 },
+          768: { slidesPerView: 3 },
           992: { slidesPerView: 4 },
         }}
       >
